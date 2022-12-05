bbc7a99b36dd0372db7d0d7c4a067054b1db76ce
DianaRomanova <romanowadia@gmail.com>
2022-12-05 19:18:37 +0000

    5_12

---
+++ 1.js
@@ -0,0 +1,6 @@
+function solution(str){
+    let n = ''
+    for(let i of str){
+      n = i+n
+    }
+    return n
---
+++ 2.js
@@ -0,0 +1,18 @@
+let a =[7,6,9,46];
+let b = a.shift();
+console.log(b);
+a.shift();
+a.shift();
+a.shift();
+console.log(a);
+let c = a.unshift(22);
+console.log(a);
+console.log(c);
+
+let d =[22,33,44,55,66,77,88,99];
+let f =d.slice(6);
+console.log(d);
+console.log(f);
+
+let e = 'hello';
+console.log(e.slice(1,4));
